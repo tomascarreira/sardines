358da75f2df0501e6611d0c5f68884437ef13391
tomascarreira <carreira_1012@hotmail.com>
2022-05-03 14:02:53 +0100

    Add check if calloc failed

--- bus.c
+++ bus.c
@@ -4,7 +4,17 @@
 bus* init_bus(mapper mapper) {
 	
 	bus* bus = calloc(1, sizeof bus);
+	if (!bus) {
+		perror("bus calloc failed.\n");
+		exit(EXIT_FAILURE);
+	}
+
 	bus->ram = calloc(1, 0x800);
+	if (!ram) {
+		perror("ram calloc failed.\n");
+		exit(EXIT_FAILURE);
+	}
+
 	bus->mapper = mapper;
 
 	return bus;
